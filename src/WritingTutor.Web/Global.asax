903c2961e6001378b970b755388861a267cc970a
dmhiggins23 <commerce@lee-higgins.com>
2015-01-22 11:18:08 -0500

    Created New Project

---
+++ src/WritingTutor.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WritingTutor.Web.MvcApplication" Language="C#" %>
---
+++ src/WritingTutor.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WritingTutor.Web.Startup))]
+namespace WritingTutor.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
